54a6b22fe0c90fd0238591589dbf64e6b038fe7a
francisco <franruiz027@gmail.com>
2020-11-25 15:45:29 +0100

    upgrades

--- app/src/main/java/com/fj/nicestart/RegisterActivity.java
+++ app/src/main/java/com/fj/nicestart/RegisterActivity.java
@@ -1,6 +1,9 @@
 package com.fj.nicestart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Button reg = findViewById(R.id.regBut);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/fj/nicestart/SplashActivity.java
+++ app/src/main/java/com/fj/nicestart/SplashActivity.java
@@ -3,6 +3,10 @@ package com.fj.nicestart;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,6 +24,16 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 2000);
+        }, 1900);
+
+        ImageView imagen = findViewById(R.id.logop);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        imagen.startAnimation(myanim);
+
+        ImageView imagen2 = findViewById(R.id.imageView3);
+        imagen2.setVisibility(View.VISIBLE);
+        Animation myanim2 =  AnimationUtils.loadAnimation(this, R.anim.mover);
+        imagen2.startAnimation(myanim2);
+
     }
 }
